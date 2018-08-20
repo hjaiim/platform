1716438f1af18d200ba855da67dc2834590ac440
haojun <hjaiim@163.com>
2018-08-20 14:23:17 +0800

    add:api文件夹

---
+++ server/routes/api/v1/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var lodash = require('lodash');
+var path = require('path');
+
+//轮询当前目录下的子模块，并挨个加载其路由配置
+fs.readdir(__dirname,(err,files)=>{
+    files.forEach((file)=>{
+        if (!lodash.startsWith(file, '.') && file !== 'index.js') {
+            try {
+              router.use('/' + file.replace('.js', ''), require('./' + file).router);
+            } catch (ex) {
+              console.error('路由加载错误[' + path.join(__dirname, file) + ']：' + ex.stack);
+            }
+          }
+    })
+})
+
+exports.router = router; 
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+//接口访问
+router.use('/api/v1', require('./api/v1/index').router);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
